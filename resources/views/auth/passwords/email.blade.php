5a1f6eeccfd0ef8fbc04b58c824603dbbd310ad5
张鹏翼 <189281351@qq.com>
2017-03-15 14:46:37 +0800

    test

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -34,7 +34,7 @@
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    发送重置链接
+                                    发送重置密码链接
                                 </button>
                             </div>
                         </div>
