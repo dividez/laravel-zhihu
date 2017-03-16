4721a4bd7a1562ee3d37fa30f925bb9add20863a
张鹏翼 <dividi_z@163.com>
2017-03-16 15:20:26 +0800

    梅花编辑器

--- resources/views/questions/create.blade.php
+++ resources/views/questions/create.blade.php
@@ -37,7 +37,17 @@
         </div>
     </div>
     <script type="text/javascript">
-        var ue = UE.getEditor('container');
+        var ue = UE.getEditor('container',{
+            toolbars: [
+                ['bold', 'italic', 'underline', 'strikethrough', 'blockquote', 'insertunorderedlist', 'insertorderedlist', 'justifyleft','justifycenter', 'justifyright',  'link', 'insertimage', 'fullscreen']
+            ],
+            elementPathEnabled: false,
+            enableContextMenu: false,
+            autoClearEmptyNode:true,
+            wordCount:false,
+            imagePopup:false,
+            autotypeset:{ indent: true,imageBlockLine: 'center' }
+        });
         ue.ready(function() {
             ue.execCommand('serverparam', '_token', '{{ csrf_token() }}'); // 设置 CSRF token.
         });
