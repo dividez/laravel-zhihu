51da673abf61a6f5f4844a04385a3dcf62531769
张鹏翼 <189281351@qq.com>
2017-03-16 11:50:01 +0800

    表单验证

---
+++ app/Http/Requests/StoreQuestionRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreQuestionRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function messages()
+    {
+        return [
+            'body.required' => '内容不能为空',
+            'body.min' => '内容不能少于25个字符',
+        ];
+    }
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required|min:6|max:196',
+            'body' => 'required|min:25',
+        ];
+    }
+}
---
+++ resources/views/public/errors.blade.php
@@ -0,0 +1,10 @@
+@if (isset($errors)&&count($errors) > 0)
+    <div class="alert alert-dismissable alert-danger">
+        <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
