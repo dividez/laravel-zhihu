5959ba7ba71fd80c0b7a74920e7bb1d4eec617e1
张鹏翼 <dividi_z@163.com>
2017-03-22 19:19:32 +0800

    vue follow

--- resources/views/questions/show.blade.php
+++ resources/views/questions/show.blade.php
@@ -33,9 +33,10 @@
                         <span>关注者</span>
                     </div>
                     <div class="panel-body">
-                        <a href="{{ route('question.follow',$question->id) }}" class="btn btn-default {{ Auth::user()->followed($question->id) ? 'btn-success' : '' }}">
+                        {{--<a href="{{ route('question.follow',$question->id) }}" class="btn btn-default {{ Auth::user()->followed($question->id) ? 'btn-success' : '' }}">
                             {{ Auth::user()->followed($question->id) ? '已关注' : '关注该问题' }}
-                        </a>
+                        </a>--}}
+                        <question-follow-button question="{{$question->id}}" user="{{Auth::id()}}"></question-follow-button>
                         <a href="#container" class="btn btn-primary">撰写答案</a>
                     </div>
                 </div>
